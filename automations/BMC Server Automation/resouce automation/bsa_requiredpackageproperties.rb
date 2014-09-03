671ae8883bba7f44ecf3869247c96f194e82bdf8
Fred <fredbreton@gmail.com>
2014-09-03 16:16:37 +0200

    better loggin

--- automations/BMC Server Automation/resouce automation/bsa_requiredpackageproperties.rb
+++ automations/BMC Server Automation/resouce automation/bsa_requiredpackageproperties.rb
@@ -19,7 +19,7 @@ ACTION_ROOT_FOLDER = "/BRPM/Actions"
 
 def execute(script_params, parent_id, offset, max_records)
 
-  raise "" if script_params["Package"].blank? && script_params["Action"].blank?
+  return [{ :title => "N.A", :key => "1", :isFolder => false, :hideCheckbox => true, :unselectable => true }] if script_params["Package"].blank? && script_params["Action"].blank?
   if script_params["Targets"]
     targets = script_params["Targets"].split(',').collect{ |t| t.split('|')} 
   elsif script_params["Target_Environment"]
@@ -30,7 +30,7 @@ def execute(script_params, parent_id, offset, max_records)
 			bquery = "SELECT * FROM \"SystemObject/Static Group/Smart Component Group\" WHERE (NAME equals \"#{script_params["SS_environment"]}\") AND (DESCRIPTION equals \"Environment\")"
 			result = BsaUtilities.get_element_systemobject(BSA_BASE_URL, BSA_USERNAME, BSA_PASSWORD, BSA_ROLE, bquery)[0] rescue nil
 		end
-		raise "Component group #{script_params["SS_environment"]} with description set to Environment cannot be found in BSA." if result.nil?
+		return [{ :title => "Component group #{script_params["SS_environment"]} with description set to Environment cannot be found in BSA.", :key => "1", :isFolder => false, :hideCheckbox => true, :unselectable => true }] if result.nil?
 		targets = [[result["groupId"], result["objectId"], result["modelType"]]]
 	else
 		targets = [script_params["Target_Environment"].split('|')]
@@ -39,11 +39,11 @@ def execute(script_params, parent_id, offset, max_records)
 		if script_params["Target_Blueprint"] == "rpm{Blueprint}"
 			blueprint = script_params["Blueprint"]
 			if blueprint.blank?
-				raise "No property Blueprint found for RPM component #{script_params["SS_component"]} or empty value"
+				return [{ :title => "No property Blueprint found for RPM component #{script_params["SS_component"]} or empty value", :key => "1", :isFolder => false, :hideCheckbox => true, :unselectable => true }]
 			else
-				bquery = "SELECT * FROM \"SystemObject/Component Template\" WHERE (NAME equals \"#{blueprint}\") AND(DESCRIPTION equals \"Blueprint\")"
+				bquery = "SELECT * FROM \"SystemObject/Component Template\" WHERE (NAME equals \"#{blueprint}\")AND(DESCRIPTION equals \"Blueprint\")"
 				blueprint = BsaUtilities.get_element_systemobject(BSA_BASE_URL, BSA_USERNAME, BSA_PASSWORD, BSA_ROLE, bquery)[0]["name"] rescue nil
-				raise "Component Template #{blueprint} with description set to Blueprint cannot be found in BSA." if blueprint.nil?
+				return [{ :title => "No property Blueprint found for RPM component #{script_params["SS_component"]} or empty value", :key => "1", :isFolder => false, :hideCheckbox => true, :unselectable => true }] if blueprint.nil?
 			end
 		else
 			blueprint = script_params["Target_Blueprint"]
@@ -52,13 +52,13 @@ def execute(script_params, parent_id, offset, max_records)
 	  blueprint = ""
 	end
   else 
-    return []
+    return [{ :title => "N.A", :key => "1", :isFolder => false, :hideCheckbox => true, :unselectable => true }]
   end
-  return [] if script_params["Package"].nil? && script_params["Action"].nil?
+  
   session_id = BsaUtilities.bsa_soap_login(BSA_BASE_URL, BSA_USERNAME, BSA_PASSWORD) 
   result = BsaUtilities.bsa_soap_assume_role(BSA_BASE_URL, BSA_ROLE, session_id) 
   target = BsaUtilities.get_component_list_from_multiselect(BSA_BASE_URL,BSA_USERNAME, BSA_PASSWORD, BSA_ROLE,targets,blueprint,1)[0]
-  mappedparams = BsaUtilities.get_component_custom_property_list(BSA_BASE_URL,BSA_USERNAME, BSA_PASSWORD, BSA_ROLE, "/id/SystemObject/Component/#{target[1]}") 
+  mappedparams = BsaUtilities.get_component_custom_property_list(BSA_BASE_URL,BSA_USERNAME, BSA_PASSWORD, BSA_ROLE, "/id/SystemObject/Component/#{target[1]}") rescue []
   pkgDBKey = script_params["Package"].split('|')[2] unless script_params["Package"].nil?
   pkgDBKey = BsaUtilities.bsa_soap_get_package_action_dbkey(BSA_BASE_URL, session_id, "#{ACTION_ROOT_FOLDER}/#{blueprint}/#{script_params["Action"]}", target[2]) unless script_params["Action"].nil?
   packageparams = BsaUtilities.bsa_soap_get_blpackage_property_list(BSA_BASE_URL, session_id, pkgDBKey)
